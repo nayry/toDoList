0ce1147b2b54303fb3177ae0492789245dba308c
Ryan <ryanwu1501@gmail.com>
2020-08-21 06:10:19 -0400

    Updates

---
+++ precache-manifest.72611c2f5af4f73c99ad908b228bd82e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96c815be21ff967c0533f291dfb2441",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "8e22f8e5f15ff88d87b3",
+    "url": "/toDoList/static/css/main.4d875ac4.chunk.css"
+  },
+  {
+    "revision": "acc475e42964551d1c8b",
+    "url": "/toDoList/static/js/2.e35cac84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoList/static/js/2.e35cac84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e22f8e5f15ff88d87b3",
+    "url": "/toDoList/static/js/main.47eeb1e1.chunk.js"
+  },
+  {
+    "revision": "ce9c0396bb5534df1642",
+    "url": "/toDoList/static/js/runtime-main.79f4ee69.js"
+  }
+]);
